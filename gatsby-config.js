5bfe3134558e400f2c858773d7c1e2ae5b12520e
pvhee <peter.vanhee@gmail.com>
2020-03-24 21:14:58 +1300

    add material UI

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,15 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: 'gatsby-plugin-material-ui',
+      // If you want to use styled components you should change the injection order.
+      options: {
+        // stylesProvider: {
+        //   injectFirst: true,
+        // },
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
