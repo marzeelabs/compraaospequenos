b351b3f1b68db0087a1832fd762f50c9a465e0f6
Luís Miguel <luism@marzeelabs.org>
2020-03-25 19:00:04 +0000

    One more file to lint

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
-const queries = require("./src/utils/algolia");
-require("dotenv").config();
+// const queries = require('./src/utils/algolia');
+require('dotenv').config();
 
 module.exports = {
   siteMetadata: {
-    title: `#compraaospequenos`,
-    description: `Covid-19 crisis: helping local shops survive and thrive.`,
-    author: `@marzeelabs`,
+    title: '#compraaospequenos',
+    description: 'Covid-19 crisis: helping local shops survive and thrive.',
+    author: '@marzeelabs',
   },
   plugins: [
     {
@@ -17,48 +17,49 @@ module.exports = {
         // },
       },
     },
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     {
-      resolve: "gatsby-source-google-spreadsheet",
+      resolve: 'gatsby-source-google-spreadsheet',
       options: {
         // The `spreadsheetId` is required, it is found in the url of your document:
         // https://docs.google.com/spreadsheets/d/<spreadsheetId>/edit#gid=0
-        spreadsheetId: "18aa2jtoNLeebymORRigaNleTB2tbIJBWIC1RwfJPYxc",
-    
+        spreadsheetId: '18aa2jtoNLeebymORRigaNleTB2tbIJBWIC1RwfJPYxc',
+
         // The `spreadsheetName` is recommended, but optional
-        // It is used as part of the id's during the node creation, as well as in the generated GraphQL-schema
-        // If you are sourcing multiple sheets, you can set this to distringuish between the source data
-        spreadsheetName: "Negocios",
-    
+        // It is used as part of the id's during the node creation, as well as in the generated
+        // GraphQL-schema. If you are sourcing multiple sheets, you can set this to distringuish
+        // between the source data.
+        spreadsheetName: 'Negocios',
+
         // The `typePrefix` is optional, default value is "GoogleSpreadsheet"
-        // It is used as part of the id's during the node creation, as well as in the generated GraphQL-schema
-        // It can be overridden to fully customize the root query
-        typePrefix: "GoogleSpreadsheet",
-    
+        // It is used as part of the id's during the node creation, as well as in the generated
+        // GraphQL-schema. It can be overridden to fully customize the root query.
+        typePrefix: 'GoogleSpreadsheet',
+
         // The `credentials` are only needed when you need to be authenticated to read the document.
         // It's an object with the following shape:
         // {
@@ -70,30 +71,32 @@ module.exports = {
         //   - https://github.com/googleapis/google-api-nodejs-client#service-to-service-authentication
         //   - https://developers.google.com/identity/protocols/OAuth2ServiceAccount
         //
-        // When you have generated your credentials, it's easiest to refer to them from an environment variable
-        // and parse it directly:
+        // When you have generated your credentials, it's easiest to refer to them from an
+        // environment variable and parse it directly:
         // credentials: JSON.parse(GOOGLE_SERVICE_ACCOUNT_CREDENTIALS),
         // credentials: GOOGLE_SERVICE_ACCOUNT_CREDENTIALS,
         credentials: {
           client_email: process.env.GOOGLE_CLOUD_CLIENT_EMAIL,
-          private_key: process.env.GOOGLE_CLOUD_PRIVATE_KEY.replace(/\_/gm, '\n'),
+          private_key: process.env.GOOGLE_CLOUD_PRIVATE_KEY.replace(/_/gm, '\n'),
         },
-    
-        // Simple node transformation during node sourcing can be achieved by implementing the following functions
+
+        // Simple node transformation during node sourcing can be achieved by implementing the
+        // following functions
         // - `filterNode`
         // - `mapNode`
         //
-        // By implementing a `filterNode(node): boolean` function, you can choose to eliminate some nodes before
-        // they're added to Gatsby, the default behaviour is to include all nodes:
+        // By implementing a `filterNode(node): boolean` function, you can choose to eliminate some
+        // nodes before they're added to Gatsby, the default behaviour is to include all nodes:
         filterNode: () => true,
-    
-        // By implementing a `mapNode(node): node` function, you can provide your own node transformations directly
-        // during node sourcing, the default implementation is to return the node as is:
-        mapNode: node => ({ 
-          ...node, 
-          'localização (distrito)': node['localização (distrito)'].split(",") 
-        })
-      }
+
+        // By implementing a `mapNode(node): node` function, you can provide your own node
+        // transformations directly during node sourcing, the default implementation is to return
+        // the node as is:
+        mapNode: node => ({
+          ...node,
+          'localização (distrito)': node['localização (distrito)'].split(','),
+        }),
+      },
     },
     // {
     //   resolve: `gatsby-plugin-algolia`,
@@ -105,4 +108,4 @@ module.exports = {
     //   },
     // },
   ],
-}
+};
