3d1b36de612cdc5f5805005af386b96f2497195e
pvhee <peter.vanhee@gmail.com>
2020-03-25 21:25:44 +1300

    cleanup - removing stale google keys (go ahead, try them if you like :+1: )

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,11 @@
 const queries = require("./src/utils/algolia");
 require("dotenv").config();
 
-GOOGLE_SERVICE_ACCOUNT_CREDENTIALS = {
-  client_email: "compra@onthemooove-1575038580315.iam.gserviceaccount.com",
-  private_key: "-----BEGIN PRIVATE KEY-----\nMIIEuwIBADANBgkqhkiG9w0BAQEFAASCBKUwggShAgEAAoIBAQDgxbQUVbrxLdmn\nQfVwXqZ6FedH0uBq6etHQ//pMxdxE0KscXJgBVpTWLqApvyybeQgXSLAghXMiu+i\nGlqxry5ZJeDTzXyPdUhiydCzODNPOG07JUvep4nr2WHx2yopHvAp3F2kzyYqaGaK\nndD79ml3j7QW/sXzzLpEmlixlhviEdJVKIL97OMTVOtPzG+IQ0cAQVke+8RiJSVd\nmamCQWW7Bm5F6oBjATRQlIxhv4LIAtF8rvHzt22x8fL1hCnNeSkihRguwGl9fT1H\nFQjjhN77xAgg6JefL8h25Js5mmSafYTlqOeK16yYW6dL4A6o13D+UHjPrpJKc5Wy\nhbJqn3aNAgMBAAECgf889lvsAoZxaZJk+y8eKkLBZn135gydYxwaOaOtcee/kwGK\nJHk9BUlfvuRyn3ofFnOoQLmnhh2MCbiNsK4EkrujQ1Y+46OrMlFbWpZXfAd0x8fr\nLnJqGCj0BWC1W/jDwRuwMleRR1LDBSbKmldXKt0oN1x2p/dKAWoZkftZ8IzzvogQ\nyYX7WZpyIqHPaKN/gWhm8CRyyfcXSBX0L79MnSNvJxLqvGjHyCN7FKbX8pR3RnnE\nBFeaO0R/4I75tXzsoWl1PQzw55VAjo6qKrmH+L/vOvXvBaJRUItUiKKxg0IKQVvf\nsCqtMdea6v1M553STwU+qBWe3VWJb7bBO9xlBOECgYEA91wBjZUexdV897MRTDLL\nD6H/xpItpD4fD11SxnjijHHR5Wtng0Tsl4F7HKpCHC/+D1LTMpiHBSp5/8Qogz38\nYN5d92FrFWlXjX80UfeZ9UmnhC8qjjIy/2Iw8EwIqbIefUWICDzoi6S6Ovdjsmfe\n4sswVvG3PlWyZmmy31reEOECgYEA6J+2saWF7RLsR7cb6IcjzpWFxoq4aXg1GIcb\nod6Pb9Onbn7nCzh9LoMRH8f8KlMJ5bxmPfud+BPrBcoDL1IDMoxwbqtIBT3Ua60E\ncOcOi4c+paWXyvpifDLgiGPtxRmDK7/4E3GmzZuZebvGYm8QkGLDdm7vyKPhJqp5\njLyzXy0CgYB94NVtfpB9bso8p2TLGSIbH36xR5xwc6iMwhoopEXt4NJG5OR1efLI\nmvzStpYUEBxDIgwWOGalq1sQXHudZkLz7NV22Z2L9OK4QB6ZA97WY5ga/YGzgTpQ\nmmB7OMZDzUAU0fiK8KwKvFaFQXX7yT40veAorrNoxmWWFn4PigduIQKBgCb83bnY\nbCHX0hIS9nJ6c93j4Y/tIAtNQljiDBeNHa04ZA945XBT/2cUQIJ7GiBd2oLOmLQ6\nv7ccX3+Q5AfCTxbhwtZJjsCSJFAHp37kuuNXv4AzyMUUQf1e6ZLybJ9kLz/d5M4d\ndj9vnMFyX0WQJ86LPwdSgI24Kbw5RjX4SnfhAoGBAKcABGdKpL/dcYjPxW0QVzs7\nre8V4+m/aSeNdKI/l1GoJAstUrMjkGSMpDCjCflIbjHs3TuH0JBEIqIUly2NRhEZ\n+W4uMYODYd6gMtCK1eX4B5d0VpH6gUH3TB8hmetidNcbNVweAEPwANQSGE+r1LIp\n0dKrbREwO+n1HzN+B60n\n-----END PRIVATE KEY-----\n",
-};
-
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `#compraaospequenos`,
+    description: `Covid-19 crisis: helping local shops survive and thrive.`,
+    author: `@marzeelabs`,
   },
   plugins: [
     {
@@ -78,7 +73,11 @@ module.exports = {
         // When you have generated your credentials, it's easiest to refer to them from an environment variable
         // and parse it directly:
         // credentials: JSON.parse(GOOGLE_SERVICE_ACCOUNT_CREDENTIALS),
-        credentials: GOOGLE_SERVICE_ACCOUNT_CREDENTIALS,
+        // credentials: GOOGLE_SERVICE_ACCOUNT_CREDENTIALS,
+        credentials: {
+          client_email: process.env.GOOGLE_CLOUD_CLIENT_EMAIL,
+          private_key: process.env.GOOGLE_CLOUD_PRIVATE_KEY,
+        },
     
         // Simple node transformation during node sourcing can be achieved by implementing the following functions
         // - `filterNode`
--- src/components/layout.js
+++ src/components/layout.js
@@ -35,9 +35,9 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          {new Date().getFullYear()}, by
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <a href="https://marzeelabs.org">Marzee Labs</a>
         </footer>
       </div>
     </>
