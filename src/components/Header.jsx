7c1647a85d7869563b8fdf59fa6089ddfd7edd7e
Jéssica Novo <50515278+JessicaNovo@users.noreply.github.com>
2020-03-27 18:19:25 +0000

    added header links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,20 @@ import { string } from 'prop-types';
 
 import { Link } from 'gatsby';
 
-import Section from 'Layouts/Section';
-
 import withClasses from 'Styles/components/header';
 
 const Header = ({ classes, siteTitle }) => (
-  <Section extraClasses={ classes } WrapperComponent="header">
-    <h1>
-      <Link className={ classes.link } to="/">
-        { siteTitle }
-      </Link>
-    </h1>
-  </Section>
+  <div className={ classes.wrapper }>
+    <Link className={ classes.title } to="/">
+      { siteTitle }
+    </Link>
+    <Link className={ classes.navigation } to="/">
+      Início
+    </Link>
+    <Link className={ classes.navigation } to="/shops">
+      Portfolio
+    </Link>
+  </div>
 );
 
 Header.propTypes = {
--- src/styles/components/header.js
+++ src/styles/components/header.js
@@ -1,13 +1,27 @@
 import wrapStyles from 'Styles/wrapStyles';
 
-const styles = theme => ({
-  root: {
-    background: theme.palette.secondary.main,
+const styles = () => ({
+
+  wrapper: {
+    background: 'transparent',
+    position: 'relative',
+    width: '100%',
+    minHeight: '40px',
+    left: '44.64px',
+    top: '10px',
+    zIndex: '15',
+    display: 'flex',
   },
 
-  link: {
-    color: theme.palette.secondary.contrastText,
+  title: {
+    color: '#ffffff',
     textDecoration: 'none',
+
+    fontWeight: '800',
+    fontSize: '20px',
+    lineHeight: '27px',
+    textAlign: 'center',
+    letterSpacing: '1px',
 
     '&:hover': {
       textDecoration: 'underline',
