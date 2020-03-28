47334fd8561b4ec49c4509cef0a1a5b4655e6299
pvhee <peter.vanhee@gmail.com>
2020-03-28 16:22:14 +1300

    remove legacy files related to Algolia

--- src/services/algolia.js
+++
@@ -1,9 +0,0 @@
-import algoliasearch from 'algoliasearch/lite';
-
-export const ALGOLIA_SEARCH_CLIENT = algoliasearch(
-  process.env.GATSBY_ALGOLIA_APP_ID,
-  process.env.GATSBY_ALGOLIA_SEARCH_KEY,
-  {
-    timeout: 30000,
-  }
-);
