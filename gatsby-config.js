ea29e28bb7d6e3ff84a318eca8b1be6424687f41
Luís Miguel <luism@marzeelabs.org>
2020-03-30 08:17:05 +0100

    Cleanup algolia stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,3 @@
-// const queries = require('./src/utils/algolia');
 require('dotenv').config();
 
 module.exports = {
@@ -121,14 +120,5 @@ module.exports = {
         },
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-algolia`,
-    //   options: {
-    //     appId: process.env.GATSBY_ALGOLIA_APP_ID,
-    //     apiKey: process.env.ALGOLIA_ADMIN_KEY,
-    //     queries,
-    //     chunkSize: 10000, // default: 1000
-    //   },
-    // },
   ],
 };
