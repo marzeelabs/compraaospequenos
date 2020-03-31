d2420fb8f3ce5b6a000df9d124d84419785c39a4
Luís Miguel <luism@marzeelabs.org>
2020-03-31 19:33:12 +0100

    Set gatsby theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -60,8 +60,8 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#2F5BEA',
+        theme_color: '#2F5BEA',
         display: 'minimal-ui',
         icon: 'src/assets/favicon.png', // This path is relative to the root of the site.
       },
