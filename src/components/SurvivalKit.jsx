2da5c628c5ac57221712412e013da8eea1aaba1a
Jéssica Novo <50515278+JessicaNovo@users.noreply.github.com>
2020-04-08 17:25:23 +0100

    #16 added survival kit section

---
+++ src/components/SurvivalKit.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { Link } from 'gatsby';
+import Typography from '@material-ui/core/Typography';
+
+import Section from 'Layouts/Section';
+
+import useStyles from 'Styles/components/survivalKit';
+
+const SurvivalKit = () => {
+  const classes = useStyles();
+
+  return (
+    <Section extraClasses={ classes }>
+      <Typography variant="h4" marked="center" className={ classes.title }>
+        Reinventa o teu negócio
+      </Typography>
+
+      <div className={ classes.descriptionWrapper }>
+        <Typography variant="body1" className={ classes.description }>
+          Vivemos tempos de enormes desafios, e de uma necessidade critica de reinventarmos
+          o nosso negócio – ou fechar as portas.
+        </Typography>
+        <Typography variant="body1" className={ classes.description }>
+          Para ajudar nesta adaptação, em parceria com as Mulheres à Obra, e com ajuda
+          de especialistas, trazemos-te um kit de sobrevivência para pequenos negócios.
+        </Typography>
+      </div>
+
+      <Link
+        className={ classes.button }
+        to="/"
+        download
+      >
+        Kit de Sobrevivência
+      </Link>
+    </Section>
+  );
+};
+
+export default SurvivalKit;
---
+++ src/styles/components/survivalKit.js
@@ -0,0 +1,76 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background,
+  },
+
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    position: 'relative',
+    maxWidth: 800,
+    marginTop: theme.spacing(12),
+    marginBottom: theme.spacing(12),
+  },
+
+  title: {
+    marginBottom: theme.spacing(5),
+    fontSize: '34px',
+    lineHeight: '46px',
+    letterSpacing: '2px',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: theme.palette.primary.main,
+  },
+
+  descriptionWrapper: {
+    fontSize: '16px',
+    lineHeight: '32px',
+    letterSpacing: '2px',
+    textAlign: 'center',
+    marginBottom: theme.spacing(3),
+  },
+
+  description: {
+    marginBottom: theme.spacing(2),
+  },
+
+  button: {
+    padding: '20px 40px',
+    borderRadius: '60px',
+    backgroundColor: theme.palette.secondary.main,
+    boxShadow: `
+      0px 3px 1px -2px rgba(0,0,0,0.2),
+      0px 2px 2px 0px rgba(0,0,0,0.14),
+      0px 1px 5px 0px rgba(0,0,0,0.12)`,
+    color: theme.palette.secondary.contrastText,
+    fontSize: '34px',
+    fontWeight: 'bold',
+    lineHeight: '40px',
+    letterSpacing: '2px',
+    textAlign: 'center',
+    textDecoration: 'none',
+    textTransform: 'none',
+    transition: `
+      background-color 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+      box-shadow 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms,
+      border 250ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;`,
+
+    [theme.breakpoints.down('sm')]: {
+      padding: '12px 24px',
+      fontSize: 22,
+    },
+
+    '&:hover': {
+      backgroundColor: theme.palette.secondary.dark,
+      boxShadow: `
+        0px 2px 4px -1px rgba(0,0,0,0.2),
+        0px 4px 5px 0px rgba(0,0,0,0.14),
+        0px 1px 10px 0px rgba(0,0,0,0.12)`,
+    },
+  },
+}));
