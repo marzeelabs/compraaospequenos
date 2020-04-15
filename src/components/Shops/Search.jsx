3d37660df8f494740e721287034596f239800bea
Luís Miguel <luism@marzeelabs.org>
2020-04-15 16:39:48 +0100

    #19 Tweak search field layout

--- src/components/Shops/Search.jsx
+++ src/components/Shops/Search.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+
 import TextField from '@material-ui/core/TextField';
 
-const ShopsSearch = ({ onChange }) => (
-  <TextField id="search" label="Search..." onChange={ onChange } />
-);
+import useStyles from 'Styles/components/shops/search';
+
+const ShopsSearch = ({ onChange }) => {
+  const classes = useStyles();
+
+  return (
+    <TextField
+      classes={ classes }
+      label={ (
+        <span className={ classes.label }>Filtrar...</span>
+      ) }
+      onChange={ onChange }
+    />
+  );
+};
 
 export default ShopsSearch;
