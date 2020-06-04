ffe64ce354de5ae73d4221294c838b4718a39561
Jéssica Novo <50515278+JessicaNovo@users.noreply.github.com>
2020-06-04 09:39:52 +0100

    #41 updated google tag manager plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -188,10 +188,6 @@ module.exports = {
       options: {
         id: 'GTM-MPPD486',
 
-        // Include GTM in development.
-        // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: true,
-
         // datalayer to be set before GTM is loaded
         // should be an object or a function that is executed in the browser
         // Defaults to null
